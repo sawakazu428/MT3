15cf31fa26e24459454c3391d91243e849a4eed3
sawada <k022g1063@g.neec.ac.jp>
2023-04-24 23:31:15 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1,20 +1,7 @@
 #include <Novice.h>
+#include "Matrix4x4.h"
 
 const char kWindowTitle[] = "GC2B_04_サワダカズキ";
-
-// 1.行列の加法
-Matrix4x4 Add
-
-// 2.行列の減法
-
-// 3.行列の積
-
-// 4.逆行列
-
-// 5.転置行列
-
-// 6.単位行列の作成
-
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -31,6 +18,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		// フレームの開始
 		Novice::BeginFrame();
 
+		Matrix4x4 m1 = { 3.2f,0.7f,9.6f,4.4f,
+		                 5.5f,1.3f,7.8f,2.1f,
+		                 6.9f,8.0f,2.6f,1.0f,
+		                 0.5f,7.2f,5.1f,3.3f };
+		Matrix4x4 m2 = { 4.1f,6.5f,3.3f,2.2f,
+						 8.8f,0.6f,9.9f,7.7f,
+						 1.1f,5.5f,6.6f,0.0f,
+						 3.3f,9.9f,8.8f,2.2f };
+
 		// キー入力を受け取る
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
@@ -39,6 +35,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Matrix4x4 resultAdd = Add(m1, m2);
+		Matrix4x4 resultMultiply = Multiply(m1, m2);
+		Matrix4x4 resultSubtract = Subtract(m1, m2);
+		Matrix4x4 inverseM1 = Inverse(m1);
+		Matrix4x4 inverseM2 = Inverse(m2);
+		Matrix4x4 transposeM1 = Transpose(m1);
+		Matrix4x4 transposeM2 = Transpose(m2);
+		Matrix4x4 identity = MakeIdentity4x4();
+
+		MatrixScreenPrintf(0, 0, resultAdd, "Add");
 		///
 		/// ↑更新処理ここまで
 		///
