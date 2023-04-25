559b6fba081830cca9de7c28756294f9c43bfe8e
sawada <k022g1063@g.neec.ac.jp>
2023-04-25 12:38:44 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <Novice.h>
-
+#include "Matrix4x4.h"
+#include "cmath"
 const char kWindowTitle[] = "GC2B_04_サワダカズキ";
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
 
+// 1. X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian)
+{
+	Matrix4x4 result;
+	result.m[0][0] = 
+}
+
+// 2, Y軸回転移動
+Matrix4x4 MakeRotateYMatrix(float radian);
+
+// 3. Z軸回転移動
+Matrix4x4 MakeRotateZMatrix(float radian);
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -11,6 +29,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+	cosf(radian);
+	std::cosf(radian);
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
