eecd2578df379d6b8ac3f2dee642057b41afb01c
sawada <k022g1063@g.neec.ac.jp>
2023-04-25 12:47:14 +0900

    a

--- main.cpp
+++ main.cpp
@@ -50,6 +50,58 @@ Matrix4x4 MakeRotateZMatrix(float radian)
 	};
 };
 
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 result;
+	result.m[0][0] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0]
+		+ m1.m[0][2] * m2.m[2][0] + m1.m[0][3] * m2.m[3][0];
+
+	result.m[0][1] = m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1]
+		+ m1.m[0][2] * m2.m[2][1] + m1.m[0][3] * m2.m[3][1];
+
+	result.m[0][2] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][2]
+		+ m1.m[0][2] * m2.m[2][2] + m1.m[0][3] * m2.m[3][2];
+
+	result.m[0][3] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][3]
+		+ m1.m[0][2] * m2.m[2][3] + m1.m[0][3] * m2.m[3][3];
+
+	result.m[1][0] = m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0]
+		+ m1.m[1][2] * m2.m[2][0] + m1.m[1][3] * m2.m[3][0];
+
+	result.m[1][1] = m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1]
+		+ m1.m[1][2] * m2.m[2][1] + m1.m[1][3] * m2.m[3][1];
+
+	result.m[1][2] = m1.m[1][0] * m2.m[0][2] + m1.m[1][1] * m2.m[1][2]
+		+ m1.m[1][2] * m2.m[2][2] + m1.m[1][3] * m2.m[3][2];
+
+	result.m[1][3] = m1.m[1][0] * m2.m[0][3] + m1.m[1][1] * m2.m[1][3]
+		+ m1.m[1][2] * m2.m[2][3] + m1.m[1][3] * m2.m[3][3];
+
+	result.m[2][0] = m1.m[2][0] * m2.m[0][0] + m1.m[2][1] * m2.m[1][0]
+		+ m1.m[2][2] * m2.m[2][0] + m1.m[2][3] * m2.m[3][0];
+
+	result.m[2][1] = m1.m[2][0] * m2.m[0][1] + m1.m[2][1] * m2.m[1][1]
+		+ m1.m[2][2] * m2.m[2][1] + m1.m[2][3] * m2.m[3][1];
+
+	result.m[2][2] = m1.m[2][0] * m2.m[0][2] + m1.m[2][1] * m2.m[1][2]
+		+ m1.m[2][2] * m2.m[2][2] + m1.m[2][3] * m2.m[3][2];
+
+	result.m[2][3] = m1.m[2][0] * m2.m[0][3] + m1.m[2][1] * m2.m[1][3]
+		+ m1.m[2][2] * m2.m[2][3] + m1.m[2][3] * m2.m[3][3];
+
+	result.m[3][0] = m1.m[3][0] * m2.m[0][0] + m1.m[3][1] * m2.m[1][0]
+		+ m1.m[3][2] * m2.m[2][1] + m1.m[3][3] * m2.m[3][0];
+
+	result.m[3][1] = m1.m[3][0] * m2.m[0][1] + m1.m[3][1] * m2.m[1][1]
+		+ m1.m[3][2] * m2.m[2][1] + m1.m[3][3] * m2.m[3][1];
+
+	result.m[3][2] = m1.m[3][0] * m2.m[0][2] + m1.m[3][1] * m2.m[1][2]
+		+ m1.m[3][2] * m2.m[2][2] + m1.m[3][3] * m2.m[3][2];
+
+	result.m[3][3] = m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3]
+		+ m1.m[3][2] * m2.m[2][3] + m1.m[3][3] * m2.m[3][3];
+	return result;
+}
+
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -61,9 +113,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+	float rotateX = MakeRotateXMatrix(radian);
 
-
-Matrix4x4 rotateMatrix = Multiply(rotate.x, Mutiply(rotate.y, rotate.z));
+Matrix4x4 rotateMatrix = Multiply(rotate.x, Multiply(rotate.y, rotate.z));
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
