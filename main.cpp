6038edf479bfb03f35e1ba5b26bda10513eeeef8
sawada <k022g1063@g.neec.ac.jp>
2023-04-25 10:29:13 +0900

    ｔ

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@ struct Vector3
 	float x, y, z;
 };
 
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label)
+{
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
 Matrix4x4 MakeTranslateMatrix(const Vector3& translate)
 {
 
