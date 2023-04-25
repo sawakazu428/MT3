8152d72352121417e7533e95d517833d273b75ed
sawada <k022g1063@g.neec.ac.jp>
2023-04-25 10:29:51 +0900

    ｔ

--- main.cpp
+++ main.cpp
@@ -7,7 +7,19 @@ struct Vector3
 {
 	float x, y, z;
 };
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
 
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix) {
+	for (int row = 0; row < 4; ++row)
+	{
+		for (int column = 0; column < 4; ++column)
+		{
+			Novice::ScreenPrintf(
+				x + column * kColumnWidth, y + row * kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
 void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label)
 {
 	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
