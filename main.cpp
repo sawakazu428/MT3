499670296de0b2c3246ef6c991d584ff29db82b8
sawada <k022g1063@g.neec.ac.jp>
2023-05-09 10:11:13 +0900

    a

--- main.cpp
+++ main.cpp
@@ -56,6 +56,50 @@ Matrix4x4 MakeScaleMatrix(const Vector3& scale)
 
 };
 
+Matrix4x4 MakeRotateXMatrix(float radian)
+{
+	float cos = std::cosf(radian);
+	float sin = std::sinf(radian);
+	Matrix4x4 result;
+	result = {
+		1.0f,0.0f,0.0f,0.0f,
+		0.0f,cos,sin,0.0f,
+		0.0f,-sin,cos,0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+	return result;
+};
+
+// 2, Y軸回転移動
+Matrix4x4 MakeRotateYMatrix(float radian)
+{
+	float cos = std::cosf(radian);
+	float sin = std::sinf(radian);
+	Matrix4x4 result;
+	result = {
+		cos,0.0f,-sin,0.0f,
+		0.0f,1.0f,0.0f,0.0f,
+		sin,0.0f,cos,0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+	return result;
+};
+
+// 3. Z軸回転移動
+Matrix4x4 MakeRotateZMatrix(float radian)
+{
+	float cos = std::cosf(radian);
+	float sin = std::sinf(radian);
+	Matrix4x4 result;
+	result = {
+		cos,sin,0.0f,0.0f,
+		-sin,cos,0.0f,0.0f,
+		0.0f,0.0f,1.0f,0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+	return result;
+};
+
 Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix)
 {
 	Vector3 result; // w=1がデカルト座標系であるので(x,y,1)のベクトルとしてmatrixとの積をとる
