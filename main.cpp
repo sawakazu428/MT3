a3581da09ed48a786c6c34053909a2148e51a011
sawada <k022g1063@g.neec.ac.jp>
2023-05-18 12:23:48 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,5 +1,60 @@
 #include <Novice.h>
+#include "Matrix4x4.h"
+#include "cmath"
+#include "Vector3.h"
+#define _USE_MATH_DEFINES
+#include "math.h"
+struct Sphere
+{
+	Vector3 center; //!< 中心点
+	float radius;   //!< 半径
+};
 
+void DrawGrid(const Matrix4x4& ViewProjectionMatrix, const Matrix4x4& viewportMatrix)
+{
+	const float kGridHalfWidth = 2.0f;                                     // Gridの半分の幅
+	const uint32_t kSubdvision = 10;                                       // 分割数
+	const float kGridEvery = (kGridHalfWidth * 2.0f) / float(kSubdvision); // 一つ分の長さ
+	// 奥から手前への線を順々に引いていく
+	for (uint32_t xIndex = 0; xIndex <= kSubdvision; ++xIndex)
+	{
+		// 上の情報を使ってワールド座標系上の始点と終点を求める
+		// スクリーン座標系まで変換をかける
+		// 変換した座標を使って表示色は薄い灰色(0xAAAAAAFF),原点は黒ぐらいが良いが、何でも良い
+		Novice::DrawLine(xIndex,xIndex,xIndex, xIndex, 0xAAAAAAFF);
+	}
+	// 左から右も同じように順々に引いていく
+	for (uint32_t zIndex = 0; zIndex <= kSubdvision; ++zIndex)
+	{
+	// 奥から手前が左右に変わるだけ
+		//Novice::DrawLine();
+	}
+
+}
+
+void DrawSphere(const Sphere& sphere, const Matrix4x4& ViewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color)
+{
+	//float pi = M_PI / 1.0f;
+	const uint32_t kSubdvision = 10; // 分割数
+	const float kLonEvery = 2.0f;    // Gridの半分の幅
+	const float kLatEvery = 2;         // 一つ分の長さ
+	// 緯度の方向に分割 -π/2 ~ π/2
+	for (uint32_t latIndex = 0; latIndex < kSubdvision; ++latIndex)
+	{
+		//float lat = -pi / 2.0f + kLatEvery * latIndex; // 現在の緯度
+		// 緯度の方向に分割θ ~ 2π
+		for (uint32_t lonIndex = 0; lonIndex < kSubdvision; ++lonIndex)
+		{
+			float lon = lonIndex * kLonEvery; // 現在の経度
+			// world座標系でのa,b,cを求める
+			Vector3 a, b, c;
+			// a,b,cをScreen座標系まで変換...
+			// ab,bcで線を引く
+			//Novice::DrawLine(color);
+			//Novice::DrawLine(color);
+		}
+	}
+}
 const char kWindowTitle[] = "GC2B_04_サワダカズキ";
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -12,6 +67,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
+	Vector3 cameraTranslate{ 0.0f,1.9f,-6.49f };
+	Vector3 cameraRotate{ 0.26f,0.0f,0.0f };
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
