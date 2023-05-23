875d8df4cdf5b82ae6a91a86b42db4ad6b9c0f9e
sawada <k022g1063@g.neec.ac.jp>
2023-05-23 10:29:03 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <Novice.h>
+#include "Vector3.h"
 
+struct Line 
+{
+	Vector3 origin; //!< 始点
+	Vector3 diff; //!< 終点への差分ベクトル
+};
+
+struct Ray
+{
+	Vector3 origin; //!< 始点
+	Vector3 diff; //!< 終点への差分ベクトル
+};
+
+struct Segment
+{
+	Vector3 origin; //!< 始点
+	Vector3 diff; //!< 終点への差分ベクトル
+};
+
+Vector3 Project(const Vector3& v1, const Vector3& v2);
+
+Vector3 ClosestPoint(const Vector3& point, const Segment& segment);
 const char kWindowTitle[] = "GC2B_04_サワダカズキ";
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -11,6 +33,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	Segment segment{ {-2.0f,-1.0f,0.0f},{3.0f,2.0f,2.0f} };
+	Vector3 point{ -1.5f,0.6f,0.6f };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -24,6 +49,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
+	Vector3 project = Project(Subtract(point, segment.origin), segment.diff);
+	Vector3 closestPoint = ClosestPoint(point, segment);
+
 
 		///
 		/// ↑更新処理ここまで
@@ -32,6 +60,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+	
+	Sphere pointSphere{ point,0.01f }; // 1cmの球を描画
+	Sphere closestPointSphere{ closestPoint,0.01f };
+	DrawSphere(pointSphere, viewProjectMatrix, viewportMatrix,RED);
+	DrawSphere(closestPointSphere, viewProjectMatrix, viewportMatrix, BLACK);
+
 
 		///
 		/// ↑描画処理ここまで
