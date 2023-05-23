c595f82d051c919259ae5890ae826532535750f3
sawada <k022g1063@g.neec.ac.jp>
2023-05-23 10:43:18 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <Novice.h>
 #include "Vector3.h"
+#include "Matrix4x4.h"
+#include "cassert"
 
 struct Line 
 {
@@ -21,7 +23,46 @@ struct Segment
 
 Vector3 Project(const Vector3& v1, const Vector3& v2);
 
-Vector3 ClosestPoint(const Vector3& point, const Segment& segment);
+Vector3 ClosestPoint(const Vector3& point, const Segment& segment)
+{
+
+};
+
+Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2)
+{
+	Matrix4x4 result;
+	result.m[0][0] = m1.m[0][0] - m2.m[0][0];
+	result.m[0][1] = m1.m[0][1] - m2.m[0][1];
+	result.m[0][2] = m1.m[0][2] - m2.m[0][2];
+	result.m[0][3] = m1.m[0][3] - m2.m[0][3];
+	result.m[1][0] = m1.m[1][0] - m2.m[1][0];
+	result.m[1][1] = m1.m[1][1] - m2.m[1][1];
+	result.m[1][2] = m1.m[1][2] - m2.m[1][2];
+	result.m[1][3] = m1.m[1][3] - m2.m[1][3];
+	result.m[2][0] = m1.m[2][0] - m2.m[2][0];
+	result.m[2][1] = m1.m[2][1] - m2.m[2][1];
+	result.m[2][2] = m1.m[2][2] - m2.m[2][2];
+	result.m[2][3] = m1.m[2][3] - m2.m[2][3];
+	result.m[3][0] = m1.m[3][0] - m2.m[3][0];
+	result.m[3][1] = m1.m[3][1] - m2.m[3][1];
+	result.m[3][2] = m1.m[3][2] - m2.m[3][2];
+	result.m[3][3] = m1.m[3][3] - m2.m[3][3];
+	return result;
+};
+
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix)
+{
+	Vector3 result; // w=1がデカルト座標系であるので(x,y,1)のベクトルとしてmatrixとの積をとる
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
+	assert(w != 0.0f); // ベクトルに対して基本的な操作を行う行列でwが0になることはありえない
+	result.x /= w; // w=1がデカルト座標系であるので、wを除算することで同次座標をデカルト座標に戻す
+	result.y /= w;
+	result.z /= w;
+	return result;
+};
 const char kWindowTitle[] = "GC2B_04_サワダカズキ";
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -36,6 +77,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	Segment segment{ {-2.0f,-1.0f,0.0f},{3.0f,2.0f,2.0f} };
 	Vector3 point{ -1.5f,0.6f,0.6f };
+
+	Vector3 start = Transform(Transform(segment.origin, viewProjectionMatrix), viewportMatrix);
+	Vector3 end = Transform(Add(Transform(segment.origin,segment.diff), viewProjectionMatrix), viewportMatrix);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -60,7 +104,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-	
+	Novice::DrawLine(int(start.x), int(start.y), int(end.x), int(end.y), WHITE);
+	ImGui::InputFloat3("Project", &project.x, "%.3f", ImguiInputTextFlags_ReadOnly);
+
 	Sphere pointSphere{ point,0.01f }; // 1cmの球を描画
 	Sphere closestPointSphere{ closestPoint,0.01f };
 	DrawSphere(pointSphere, viewProjectMatrix, viewportMatrix,RED);
