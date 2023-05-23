c0642ee50abd6e8742152d2666a3dc977143fd90
sawada <k022g1063@g.neec.ac.jp>
2023-05-23 10:48:36 +0900

    a

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "Matrix4x4.h"
 #include "cassert"
 
+
 struct Line 
 {
 	Vector3 origin; //!< 始点
@@ -21,11 +22,35 @@ struct Segment
 	Vector3 diff; //!< 終点への差分ベクトル
 };
 
-Vector3 Project(const Vector3& v1, const Vector3& v2);
+Vector3 Project(const Vector3& v1, const Vector3& v2)
+{
+
+}
 
 Vector3 ClosestPoint(const Vector3& point, const Segment& segment)
 {
 
+};
+
+Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 result;
+	result.m[0][0] = m1.m[0][0] + m2.m[0][0];
+	result.m[0][1] = m1.m[0][1] + m2.m[0][1];
+	result.m[0][2] = m1.m[0][2] + m2.m[0][2];
+	result.m[0][3] = m1.m[0][3] + m2.m[0][3];
+	result.m[1][0] = m1.m[1][0] + m2.m[1][0];
+	result.m[1][1] = m1.m[1][1] + m2.m[1][1];
+	result.m[1][2] = m1.m[1][2] + m2.m[1][2];
+	result.m[1][3] = m1.m[1][3] + m2.m[1][3];
+	result.m[2][0] = m1.m[2][0] + m2.m[2][0];
+	result.m[2][1] = m1.m[2][1] + m2.m[2][1];
+	result.m[2][2] = m1.m[2][2] + m2.m[2][2];
+	result.m[2][3] = m1.m[2][3] + m2.m[2][3];
+	result.m[3][0] = m1.m[3][0] + m2.m[3][0];
+	result.m[3][1] = m1.m[3][1] + m2.m[3][1];
+	result.m[3][2] = m1.m[3][2] + m2.m[3][2];
+	result.m[3][3] = m1.m[3][3] + m2.m[3][3];
+	return result;
 };
 
 Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2)
