14da8ada17a68cb446fb33286b6f2f1f11026a71
sawada <k022g1063@g.neec.ac.jp>
2023-05-29 17:05:46 +0900

    a

--- main.cpp
+++ main.cpp
@@ -352,6 +352,107 @@ Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, f
 
 	return result;
 }
+
+void DrawGrid(const Matrix4x4& ViewProjectionMatrix, const Matrix4x4& viewportMatrix)
+{
+	const float kGridHalfWidth = 2.0f;                                     // Gridの半分の幅
+
+	const uint32_t kSubdvision = 10;                                       // 分割数
+
+	const float kGridEvery = (kGridHalfWidth * 2.0f) / float(kSubdvision); // 一つ分の長さ
+
+	// 奥から手前への線を順々に引いていく
+	for (uint32_t xIndex = 0; xIndex <= kSubdvision; ++xIndex)
+	{
+		// 上の情報を使ってワールド座標系上の始点と終点を求める
+		float x = -kGridHalfWidth + (xIndex * kGridEvery);
+
+		// スクリーン座標系まで変換をかける
+		Vector3 start{ x,0.0f, -kGridHalfWidth };
+
+		Vector3 startScreen = Transform(Transform(start, ViewProjectionMatrix), viewportMatrix);
+
+		Vector3 end{ x,0.0f,kGridHalfWidth };
+
+		Vector3 endScreen = Transform(Transform(end, ViewProjectionMatrix), viewportMatrix);
+
+		// 変換した座標を使って表示色は薄い灰色(0xAAAAAAFF),原点は黒ぐらいが良いが、何でも良い
+		Novice::DrawLine(
+			int(startScreen.x), int(startScreen.y), int(endScreen.x), int(endScreen.y),
+			x == 0.0f ? BLACK : 0xAAAAAAFF);
+	}
+
+	// 左から右も同じように順々に引いていく
+	for (uint32_t zIndex = 0; zIndex <= kSubdvision; ++zIndex)
+	{
+		// 上の情報を使ってワールド座標系上の始点と終点を求める
+		float z = -kGridHalfWidth + (zIndex * kGridEvery);
+
+		Vector3 start{ -kGridHalfWidth,0.0f,z };
+
+		Vector3 end{ kGridHalfWidth,0.0f,z };
+
+		// スクリーン座標系まで変換をかける
+		Vector3 startScreen = Transform(Transform(start, ViewProjectionMatrix), viewportMatrix);
+		Vector3 endScreen = Transform(Transform(end, ViewProjectionMatrix), viewportMatrix);
+
+		// 変換した座標を使って表示色は薄い灰色(0xAAAAAAFF),原点は黒ぐらいが良いが、何でも良い
+		Novice::DrawLine(
+			int(startScreen.x), int(startScreen.y), int(endScreen.x), int(endScreen.y),
+			z == 0.0f ? BLACK : 0xAAAAAAFF);
+
+	}
+
+}
+
+void DrawSphere(const Sphere& sphere, const Matrix4x4& ViewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color)
+{
+	float pi = std::numbers::pi_v<float>;
+	const uint32_t kSubdvision = 12; // 分割数
+	const float kLonEvery = pi * 2.0f / float(kSubdvision);    // Gridの半分の幅
+	const float kLatEvery = pi / float(kSubdvision);         // 一つ分の長さ
+
+	// 緯度の方向に分割 -π/2 ~ π/2
+	for (uint32_t latIndex = 0; latIndex < kSubdvision; ++latIndex)
+	{
+		float lat = -pi / 2.0f + kLatEvery * latIndex; // 現在の緯度
+		// 緯度の方向に分割θ ~ 2π
+		for (uint32_t lonIndex = 0; lonIndex < kSubdvision; ++lonIndex)
+		{
+			float lon = lonIndex * kLonEvery; // 現在の経度
+			// world座標系でのa,b,cを求める
+			Vector3 a =
+			{
+				sphere.center.x + sphere.radius * std::cos(lat) * std::cos(lon),
+				sphere.center.y + sphere.radius * std::sin(lat),
+				sphere.center.z + sphere.radius * std::cos(lat) * std::sin(lon)
+			};
+			Vector3 b =
+			{
+				sphere.center.x + sphere.radius * std::cos(lat + kLatEvery) * std::cos(lon),
+				sphere.center.y + sphere.radius * std::sin(lat + kLatEvery),
+				sphere.center.z + sphere.radius * std::cos(lat + kLatEvery) * std::sin(lon)
+
+			};
+			Vector3 c =
+			{
+				sphere.center.x + sphere.radius * std::cos(lat) * std::cos(lon + kLonEvery),
+				sphere.center.y + sphere.radius * std::sin(lat),
+				sphere.center.z + sphere.radius * std::cos(lat) * std::sin(lon + kLonEvery)
+			};
+
+			// a,b,cをScreen座標系まで変換...
+			Vector3 screenA = Transform(Transform(a, ViewProjectionMatrix), viewportMatrix);
+			Vector3 screenB = Transform(Transform(b, ViewProjectionMatrix), viewportMatrix);
+			Vector3 screenC = Transform(Transform(c, ViewProjectionMatrix), viewportMatrix);
+
+			// ab,bcで線を引く
+			Novice::DrawLine(int(screenA.x), int(screenA.y), int(screenB.x), int(screenB.y), color);
+			Novice::DrawLine(int(screenB.x), int(screenB.y), int(screenC.x), int(screenC.y), color);
+		}
+	}
+}
+
 const char kWindowTitle[] = "GC2B_04_サワダカズキ";
 
 // Windowsアプリでのエントリーポイント(main関数)
